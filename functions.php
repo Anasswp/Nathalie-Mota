ebc9409ba4348729f847eefda569305ca31e05fd
Anass.b <85401195+Anasswp@users.noreply.github.com>
2023-12-18 19:20:19 +0100

    main

--- functions.php
+++ functions.php
@@ -3,7 +3,11 @@
 function theme_enqueue_styles()
 {
     wp_enqueue_style('parent-style', get_template_directory_uri(). '/style.css');
+    
 }
+
+add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
+
 
 /* Enregistrement du menu principal */
 function register_custom_menus() {
