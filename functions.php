6b78c16807e69b5a31fc19b9d9a69f094ad4fc06
Anass.b <85401195+Anasswp@users.noreply.github.com>
2023-12-18 19:40:30 +0100

    main

--- functions.php
+++ functions.php
@@ -11,8 +11,8 @@ add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
 /* Enregistrement du menu principal */
 function register_custom_menus() {
     register_nav_menus(array(
-        'menu_principal' => __('header', 'Photographe'),
-        'menu_secondaire' => __('footer', 'Photographe'),
+        'menu_principal' => __('header', 'Nathalie Mota'),
+        'menu_secondaire' => __('footer', 'Nathalie Mota'),
     ));
 }
  
