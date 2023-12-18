0acaaae7defbf9ceaf50493d3ac8b5d1f9bae93b
Anass.b <85401195+Anasswp@users.noreply.github.com>
2023-12-18 19:06:59 +0100

    main

--- functions.php
+++ functions.php
@@ -1,6 +1,5 @@
 <?php
-/*Chargement de 'style.css pour le bon fonctionnement du theme*/
-add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
+/*Chargement du style et des scripts pour le bon fonctionnement du theme*/
 function theme_enqueue_styles()
 {
     wp_enqueue_style('parent-style', get_template_directory_uri(). '/style.css');
