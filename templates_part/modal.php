e8f6b9d95ff8cc224efd6ba0f1d8d23754f256e2
Anass.b <85401195+Anasswp@users.noreply.github.com>
2023-12-19 16:24:34 +0100

    main

--- templates_part/modal.php
+++ templates_part/modal.php
@@ -16,7 +16,7 @@
 				if (get_field('reference')) {
 					$refPhoto = get_field('reference');
 				}; 
-				echo do_shortcode('[contact-form-7 id="32" title="Formulaire de contact"]');
+				echo do_shortcode('[]');
 			?>
 		</div>	
 	</div>
