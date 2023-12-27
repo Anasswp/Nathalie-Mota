1e7c35c7cf45a8415f16d408527f66802371cb45
Anass.b <85401195+Anasswp@users.noreply.github.com>
2023-12-27 14:46:09 +0100

    main

--- home.php
+++ home.php
@@ -1,5 +1,13 @@
-
-<!-- On appelle le header et le footer-->
 <?php get_header(); ?>
+
+<div id="front-page"> 
+    <section id="content">    
+        <!-- Chargement du hero -->
+        <?php get_template_part( 'templates_part/hero.php' ); ?>
+    </section>
+</div>
+
+
+
 
 <?php get_footer(); ?>
