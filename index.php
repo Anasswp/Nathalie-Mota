69cac2ed89238167e847322e17ee91ac8585c5d6
Anass.b <85401195+Anasswp@users.noreply.github.com>
2023-12-28 18:08:06 +0100

    main

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php get_header() ?>
 
+    <div>
+    <?php 
+        /*On appelle la modale dans notre header*/
+        include('templates_part/hero.php');
+        ?>
+    </div>
+
 <?php get_footer() ?>
