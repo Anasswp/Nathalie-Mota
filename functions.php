5eac35f1dcd6ad5513cbdc2553c2a91b50b07dd4
Anass.b <85401195+Anasswp@users.noreply.github.com>
2024-01-03 12:46:29 +0100

    main

--- functions.php
+++ functions.php
@@ -15,8 +15,8 @@ add_action('wp_enqueue_scripts', 'theme_enqueue_styles_scripts');
 function theme_enqueue_styles_custom()
 {
     // Chargement des styles
-    wp_enqueue_style('style-custom', get_template_directory_uri() . '/style.css');
-    wp_enqueue_style('theme-custom', get_template_directory_uri() . '/assets/css/theme.css');
+    wp_enqueue_style('style-custom', get_template_directory_uri() . '/style.scss');
+    wp_enqueue_style('theme-custom', get_template_directory_uri() . '/assets/sass/theme.scss');
     wp_enqueue_style('single-custom', get_stylesheet_directory_uri() . '/assets/css/single.css');
 
     
